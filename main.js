a2474cbcf93eb01c43d465a61b040de95b85f22b
ThornSteel <martinpowerqwerty@gmail.com>
2024-03-06 22:43:19 -0300

    completo

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ function mostrarFrasesConocer(selector1, contenedorPersona, botonSelector) {
     if (!primeroMostrado) {
       contenedorMostrado = true;
       contenedor.style.display = "block";
+      
 
       botonSiguiente.addEventListener("click", elGuiaVikingo);
 
@@ -32,23 +33,75 @@ function mostrarFrasesConocer(selector1, contenedorPersona, botonSelector) {
         let nombre = document.querySelector("#nombre").value;
         let razon = document.querySelector("#razon").value;
         let nacionalidad = document.querySelector("#nacionalidad").value;
-        let placer = document.querySelector("#contenedorPlacer");
         let informacionPersonaLs;
 
-        if (nombre === "" || razon === "" || nacionalidad === "") {
-          alert("Por favor complete los campos");
-        } else {
-          let informacionPersona = new Persona(nombre, razon, nacionalidad);
-          localStorage.setItem("InformacionPersona", JSON.stringify(informacionPersona));
-          informacionPersonaLs = JSON.parse(localStorage.getItem("InformacionPersona"));
-
-          alert("");
-        }
-        placer.style.display = "block";
-        placer.innerHTML = "es un placer conocerte " + informacionPersonaLs.nombrePersona;
+        (nombre === "" || razon === "" || nacionalidad === "") ? (
+          event.preventDefault(),
+          Swal.fire({
+            position: "top",
+            icon: "warning",
+            title: "Por favor complete los campos",
+            timer: 1500
+          })
+        ) : (
+          (() => {
+            let informacionPersona = new Persona(nombre, razon, nacionalidad);
+            localStorage.setItem("InformacionPersona", JSON.stringify(informacionPersona));
+            informacionPersonaLs = JSON.parse(localStorage.getItem("InformacionPersona"));
+          })()
+        );
+        
       }
     }
   });
 }
 
 mostrarFrasesConocer("#primero", "#inputsPersona", "#boton");
+
+ScrollReveal().reveal(".widget", { interval: 400 });
+
+/* Codigo para la promesa */
+
+let listaDeTecnologias = [
+  { nombre: "bootStrap", imagen: "img/BootStrap-removebg-preview.png" },
+  { nombre: "JavaScript", imagen: "img/JavaScript-removebg-preview.png" },
+  { nombre: "Html", imagen: "img/html-removebg-preview.png" },
+  { nombre: "Css", imagen: "img/CSS-removebg-preview.png" },
+  { nombre: "SASS", imagen: "img/CSS-removebg-preview.png" },
+  { nombre: "GitHub", imagen: "img/CSS-removebg-preview.png" },
+];
+
+/* Promesa  */
+const obtenerTecnologias = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(listaDeTecnologias);
+    }),
+      0;
+  });
+};
+
+/* renderizar tarjetas de tecnologia */
+
+function renderizarImagen(x) {
+  let = contenedorTarjetas = ""
+  for (const tarjeta of x) {
+    contenedorTarjetas += `
+    <div class="contenedorTarjetaTecnologias">
+      <div class="contenedorImgTecnologias">
+        <img src="${tarjeta.imagen}" alt="${tarjeta.nombre}" />
+      </div>
+      <div class="contenedorDescripcion">
+        <p>${tarjeta.nombre}</p>
+     </div>
+    </div>
+    `
+  }
+  document.querySelector(".contenedorTecnologias").innerHTML = contenedorTarjetas
+}
+function mostrarTarjetas() {
+  obtenerTecnologias().then((data) => {
+    renderizarImagen(data);
+  });
+}
+mostrarTarjetas()
